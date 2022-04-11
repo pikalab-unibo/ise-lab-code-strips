87e371853e8006a23070195552798c3fd589fb9c
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-04-11 14:34:01 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+rootProject.name = "ise-lab-code-template"
+include("exercise")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
